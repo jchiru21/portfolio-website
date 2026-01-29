9c7e9a1e3c8bcbeb917ecfc9cafddd0841d1cc20
Chiranjeevi J <chiranjeevi@192.168.1.4>
2026-01-29 11:04:38 +0530

    Add Vercel Web Analytics to track visitors and page views

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 // pages/_app.js
 import { ThemeProvider } from "next-themes"
+import { Analytics } from "@vercel/analytics/next"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="portfolio-theme">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
